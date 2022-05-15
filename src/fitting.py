964f10796f9b6d34d9450a8090c412829ff3355b
Jinchan Seo <sjhmp21@hanyang.ac.kr>
2022-05-16 03:14:29 +0900

    ãadd fitting and IV module

---
+++ src/fitting.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def IV_Fitting(v, i_s, q, nk, cst, v_list=[], c_list=[]):
+    polyfit_iv = np.polyfit(v_list, c_list, 12)
+    fit_iv = np.poly1d(polyfit_iv)
+    return abs(i_s * (np.exp(q * v / nk) - 1)) + cst * fit_iv(v)
