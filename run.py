05096aa141fd76b2ca939f365ef155862e5900d7
Jinchan Seo <sjhmp21@hanyang.ac.kr>
2022-05-18 20:46:41 +0900

    modify extract file

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ sys.path.append('./src')
 from filter import *
 from IV_data import *
 from IV_plot import *
-from graph import *
 from extract import *
+from Tm_data import *
+from Tm_plot import *
 
-
+print(LMZ_files)
--- src/Tm_plot.py
+++ src/Tm_plot.py
@@ -109,15 +109,3 @@ def graph_(x, savefile=False):
     if savefile == True:
         plt.savefig(f'{file_name}.png', dpi=400)
 
-
-if __name__ == '__main__':
-
-    LMZ_file = glob.glob('C:/Users/김찬영/PycharmProjects/pythonProject/Team-A2/dat/**/*LMZ?.xml', recursive=True)
-    for i, file in enumerate(LMZ_file):
-        print(i, '.', file)
-    index = int(input('Please select the number of the file. ex. 0 '))
-    save = input('Will you save it? (y/n) ')
-    if save == 'y':
-        graph_(LMZ_file[index], savefile=True)
-    else:
-        graph_(LMZ_file[index], savefile=False)
