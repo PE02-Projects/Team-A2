f475395f616847a559cd7058bd71218acc4e6c44
Jinchan Seo <sjhmp21@hanyang.ac.kr>
2022-05-26 11:58:13 +0900

    modify launcher

--- run.py
+++ run.py
@@ -2,12 +2,3 @@ import sys
 sys.path.append('./src')
 
 from get_result import *
-
-# from UI import *
-#
-# while True:
-#     if __name__ == '__main__':
-#         app = QApplication(sys.argv)
-#         win = MyWindow()
-#         win.show()
-#         sys.exit(app.exec_())
